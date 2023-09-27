aa44ecc1308a80f7a13edc220e76f86fb85bd288
MauricioAlonso <program.alonso@gmail.com>
2023-09-27 17:38:07 -0300

    los productos se cargarn en el contenedor

--- src/public/assets/scripts/addToCart.js
+++ src/public/assets/scripts/addToCart.js
@@ -1,44 +1,30 @@
-// Escucha el evento de clic en el botón "Agregar al Carrito"
-document.addEventListener("click", function (event) {
-  // Verifica si se hizo clic en un botón con la clase "agregar-al-carrito"
-  if (event.target.classList.contains("agregar-al-carrito")) {
-    // Obtiene el ID del producto desde el atributo de datos del botón
-    const productId = event.target.dataset.productid;
+// Función para agregar un producto al carrito
+function agregarAlCarrito(productId, productName) {
+  // Puedes realizar lógica para agregar el producto al carrito aquí
+  // Por ejemplo, puedes mantener un arreglo de productos en el carrito
 
-    // Obtiene la cantidad deseada del producto (puedes obtenerla de un input)
-    const quantity = parseInt(prompt("Ingrese la cantidad deseada:"));
+  // Agregar el producto al contenedor del carrito
+  const carritoContainer = document.getElementById('carritoProductos');
+  const productoDiv = document.createElement('div');
+  productoDiv.textContent = productName;
+  carritoContainer.appendChild(productoDiv);
+}
 
-    // Verifica si la cantidad es válida
-    if (isNaN(quantity) || quantity <= 0) {
-      alert("La cantidad ingresada no es válida.");
-      return;
-    }
+// Obtén todos los elementos <a> con el atributo data-product-id
+const addToCartLinks = document.querySelectorAll('a[data-product-id]');
 
-    // Crea un objeto con los datos del producto a agregar al carrito
-    const productToAdd = {
-      id: productId,
-      quantity: quantity,
-    };
+// Agrega un evento de clic a cada enlace "Agregar al Carrito"
+addToCartLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); // Evita la navegación a la URL "#" por defecto
 
-    // Realiza una solicitud POST al servidor para agregar el producto al carrito
-    fetch("/api/agregar-al-carrito", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(productToAdd),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          alert("Producto agregado al carrito con éxito.");
-        } else {
-          alert("Error al agregar el producto al carrito.");
-        }
-      })
-      .catch((error) => {
-        console.error("Error al realizar la solicitud:", error);
-        alert("Hubo un error al conectar con el servidor.");
-      });
-  }
+    // Obtiene el ID del producto desde el atributo data-product-id
+    const productId = link.getAttribute('data-product-id');
+
+    // Obtén el nombre del producto (puedes obtenerlo de donde sea necesario)
+    const productName = link.getAttribute('data-product-name');
+
+    // Agrega el producto al carrito y actualiza la vista del carrito
+    agregarAlCarrito(productId, productName);
+  });
 });
