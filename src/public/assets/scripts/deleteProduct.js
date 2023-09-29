81598430015699a429e2cc639687f7496512e225
MauricioAlonso <program.alonso@gmail.com>
2023-09-28 21:06:45 -0300

    testeado el codigo

--- src/public/assets/scripts/deleteProduct.js
+++ src/public/assets/scripts/deleteProduct.js
@@ -1,4 +1,33 @@
-// Obtén todos los enlaces "Eliminar" por su clase "eliminar-producto"
+const eliminarBotones = document.querySelectorAll(".eliminar-producto");
+
+eliminarBotones.forEach((enlace) => {
+  enlace.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const productId = this.getAttribute("data-product-id");
+
+    // Comentar la siguiente línea de alerta
+    // alert(`¿Estás seguro de que deseas eliminar el producto con ID: ${productId}?`);
+
+    fetch(`/api/products/${productId}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          window.location.href = "/DBproducts";
+        } else {
+          // Manejar errores de eliminación si es necesario
+        }
+      })
+      .catch((error) => {
+        // Manejar errores de red u otros errores aquí
+        console.error("Error al eliminar el producto:", error);
+      });
+  });
+});
+
+/*
+
 const eliminarBotones = document.querySelectorAll(".eliminar-producto");
 
 eliminarBotones.forEach((enlace) => {
@@ -19,3 +48,4 @@ eliminarBotones.forEach((enlace) => {
       .catch((error) => {});
   });
 });
+*/
